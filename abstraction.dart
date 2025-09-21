c95faa693e478898a062d297860b3790a26c307c
Talal <muhammadtalal780@gmail.com>
2025-09-21 13:01:07 +0500

    abstraction concept

---
+++ abstraction.dart
@@ -0,0 +1,11 @@
+// 🔹 Simple Explanation
+
+// Abstraction ka matlab hai → sirf important cheezen dikhana, details chhupana.
+
+// 👉 Socho tum car chalate ho:
+
+// Tumhe bas steering, accelerator, brake dikhte hain.
+
+// Tumhe engine ke internal parts kaam kaise karte hain, yeh nahi dikhte.
+
+// Yehi abstraction hai.
