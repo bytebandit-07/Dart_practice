fb02e5f091b1d7e651899a4e6c1daa9c2c881948
Talal <muhammadtalal780@gmail.com>
2025-10-04 10:46:15 +0500

    Update list_methods.dart

--- Lists_in_dart/list_methods.dart
+++ Lists_in_dart/list_methods.dart
@@ -1,4 +1,5 @@
 void main(){
+  
   //initial List
   List<String> players = ['Babar', 'Imad', 'Amir', 'Malik'];
   print('Initial List: $players\n');
@@ -14,16 +15,21 @@ void main(){
   //.insert()
   players.insert(2, "faizan");
   print('After . addInsert(): $players\n');
+  
   // .remove
   players.remove("shahdab");
   print('After . addremove(): $players\n');
+  
   //.removeAt
   players.removeAt(7);
   print('After . addremoveAt(): $players\n');
+  
   //.length
   print('Number of players: ${players.length}\n');
+  
   //.cotains()
   print('is Babar is still in List: ${players.contains("Babar")}\n');  
+  
   //.clear
   players.clear();
   print("After clear(): $players\n");
