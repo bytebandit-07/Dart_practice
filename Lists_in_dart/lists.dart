3421915108ab93c928ffad6bf3093e4a72645b0d
Talal <muhammadtalal780@gmail.com>
2025-10-04 10:21:50 +0500

    use List.filled, .generate.

--- Lists_in_dart/lists.dart
+++ Lists_in_dart/lists.dart
@@ -6,5 +6,14 @@ print(students[1]);
 List<int> numbers = [123, 4342, 232, 2321];
 print(students[3]);
 
+//empty lists
+var seats = List.filled(4, 'Empty Seat');
+print(seats);
 
+List<int> scoreBoard = List.filled(5, 0);
+print(scoreBoard);
+
+//list.generate
+List<int> square = List.generate(3, (index) => index*index);
+print(square);
 }
