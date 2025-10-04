550035843a2cadc882f0544c1734169db6eb12fb
Talal <muhammadtalal780@gmail.com>
2025-10-04 11:22:15 +0500

    adding more methods

--- Lists_in_dart/modernList_method.dart
+++ Lists_in_dart/modernList_method.dart
@@ -14,10 +14,20 @@ void main(){
   print('Names starting with A: $namesWithA\n');
 
   // map() → transform each element into something new
-   print('map():');
-   List<String> emails = students
+  print('map():');
+  List<String> emails = students
    .map((name) => '${name.toLowerCase()}@cui.edu.pk')
    .toList();
   print("Generated emails: $emails\n");
+
   
+  //  any() → check if at least one matches condition
+  print('any():');
+  bool hasSana = students.any((n) => n == "Sana");
+  print("is Sanan is in the List? $hasSana\n");
+
+  
+  //  every() → check if all match condition
+  print('every():');
+
 }
